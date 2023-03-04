62c1590dab92fd55199b4411a75f79c0e8c13eb4
kaufman <hellokaufman@gmail.com>
2023-03-03 19:11:49 -0600

    updated

--- bases.sql
+++ bases.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 04, 2023 at 02:00 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sa-mp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bases`
---
 
 CREATE TABLE `bases` (
   `id` int(11) NOT NULL,
@@ -35,17 +12,6 @@ CREATE TABLE `bases` (
   `cppos` varchar(24) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bases`
---
 ALTER TABLE `bases`
   ADD PRIMARY KEY (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
